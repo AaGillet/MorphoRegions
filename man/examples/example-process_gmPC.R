7825dffd638fb70620ab707daa66306f569eca31
Noah Greifer <noah.greifer@gmail.com>
2026-06-05 16:47:09 -0400

    Doc, vignette, and metadata updates

--- man/examples/example-process_gmPC.R
+++ man/examples/example-process_gmPC.R
@@ -2,16 +2,16 @@
 data("seal_gmm")
 
 # Process PC scores and morphological data:
-seal_pca <- process_gmPC(data=seal_gmm$coord_gpa,
-                         pcscores=seal_gmm$scores,
+seal_pca <- process_gmPC(data = seal_gmm$coord_gpa,
+                         pcscores = seal_gmm$scores,
                          eigenvals = seal_gmm$eigenvals)
 
 # Process multiple datasets; specimen names in 'specimens' vector, vertebrae index in 'vertebrae' vector
 data("wolves_gmm")
 
 # Process PC scores and geometric morphometric dataset:
-wolves_pca <- process_gmPC(data=wolves_gmm$coord_gpa,
-                           pos=wolves_gmm$vertebrae,
-                           pcscores=wolves_gmm$scores,
+wolves_pca <- process_gmPC(data = wolves_gmm$coord_gpa,
+                           pos = wolves_gmm$vertebrae,
+                           pcscores = wolves_gmm$scores,
                            eigenvals = wolves_gmm$eigenvals,
-                           specimens=wolves_gmm$specimens)
+                           specimens = wolves_gmm$specimens)
